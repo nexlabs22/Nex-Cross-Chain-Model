b8d366f0d3ac54d283069b9d4f6b926baa8e4900
Walt <waltschagen@hotmail.com>
2023-11-07 22:52:36 +0100

    first commit to test branch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,18 +10,19 @@ import Footer from '@components/Footer'
 
 const Dashboard: NextPage = () => {
 	return (
-			<main className="m-0 h-screen w-screen bg-whiteBackground-500 p-0 overflow-x-hidden">
-				<DappNavbar />
-				<TopIndexData />
-				<div className='w-full h-10 bg-transparent'></div>
-				<Footer />
-			</main>
+		<main className="m-0 h-screen w-screen bg-whiteBackground-500 p-0 overflow-x-hidden">
+			Test branch
+			<DappNavbar />
+			<TopIndexData />
+			<div className="w-full h-10 bg-transparent"></div>
+			<Footer />
+		</main>
 	)
 }
 
 export const metadata: Metadata = {
-    title: 'Dashboard',
-    description: 'Overview of spot index trading products',
+	title: 'Dashboard',
+	description: 'Overview of spot index trading products',
 }
 
 export default Dashboard
