66fa3dd6ad085ec9c3faebd6c267381928142c30
Walt <waltschagen@hotmail.com>
2023-11-14 20:05:38 +0100

    update metadata shortcut icon in Head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,24 @@
 import { type NextPage } from 'next'
-import { Metadata } from 'next'
-
-// Components
-
-// import Navbar from "@components/Navbar";
 import DappNavbar from '@components/DappNavbar'
 import TopIndexData from '@components/dashboard/TopIndexData'
 import Footer from '@components/Footer'
+import Head from 'next/head'
 
 const Dashboard: NextPage = () => {
 	return (
-		<main className="m-0 h-screen w-screen bg-whiteBackground-500 p-0 overflow-x-hidden">
-			Test branch, small change again
-			<DappNavbar />
-			<TopIndexData />
-			<div className="w-full h-10 bg-transparent"></div>
-			<Footer />
-		</main>
+		<>
+			<Head>
+				<title>Dashboard</title>
+				<meta name="description" content="Overview of spot index trading products" />
+			</Head>
+			<main className="m-0 h-screen w-screen bg-whiteBackground-500 p-0 overflow-x-hidden">
+				<DappNavbar />
+				<TopIndexData />
+				<div className="w-full h-10 bg-transparent"></div>
+				<Footer />
+			</main>
+		</>
 	)
-}
-
-export const metadata: Metadata = {
-	title: 'Dashboard',
-	description: 'Overview of spot index trading products',
 }
 
 export default Dashboard
