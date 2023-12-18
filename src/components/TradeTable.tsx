fe09debf6cceb4414c4e230f7bffe344fe6480fe
Naman Thitha <namanthitha01@gmail.com>
2023-12-19 00:51:00 +0530

    Integrated Portfolio History Table

--- src/components/TradeTable.tsx
+++ src/components/TradeTable.tsx
@@ -1,22 +1,36 @@
-import { goerliAnfiFactory, goerliAnfiV2Factory } from '@/constants/contractAddresses'
+import { goerliAnfiFactory, goerliAnfiV2Factory, goerliCrypto5Factory } from '@/constants/contractAddresses'
 import { GetPositionsHistory } from '@/hooks/getTradeHistory'
 import { GetPositionsHistory2 } from '@/hooks/getTradeHistory2'
 import { FormatToViewNumber } from '@/hooks/math'
 import useTradePageStore from '@/store/tradeStore'
-import React, { useEffect } from 'react'
+import { Positions } from '@/types/tradeTableTypes'
+import React, { useEffect, useState } from 'react'
 
 function HistoryTable() {
 
 
 	const { isFromCurrencyModalOpen, isToCurrencyModalOpen, setFromCurrencyModalOpen, setToCurrencyModalOpen, changeSwapFromCur, changeSwapToCur, swapFromCur, swapToCur, nftImage, setNftImage, setTradeTableReload, tradeTableReload } =
 		useTradePageStore()
+	const positionHistory = GetPositionsHistory2()
 
-
-	// const positionHistory = GetPositionsHistory(swapToCur.factoryAddress as `0x${string}`, swapToCur.Symbol)
-	// const positionHistory = GetPositionsHistory2(swapToCur.factoryAddress as `0x${string}`, swapToCur.Symbol)
-	// useEffect(() => {
-	const positionHistory = GetPositionsHistory2(goerliAnfiV2Factory as `0x${string}`, 'ANFI')
+	const [positionHistoryData, setPositionHistoryData] = useState<Positions[]>([]);
+	const path = window.location.pathname
 	useEffect(() => {
+		const allowedSymbols = ['ANFI', 'CRYPTO5'];
+		const activeTicker = [swapFromCur.Symbol, swapToCur.Symbol].filter(symbol => allowedSymbols.includes(symbol));
+		if (path === '/trade') {
+			const data = positionHistory.data.filter((data) => {
+				return activeTicker.includes(data.indexName)
+			})
+			setPositionHistoryData(data)
+		} else if (path === '/portfolio') {
+			setPositionHistoryData(positionHistory.data)
+		}
+	}, [path, positionHistory.data, swapFromCur.Symbol, swapToCur.Symbol])
+
+
+	useEffect(() => {
+
 		if (tradeTableReload) {
 			positionHistory.reload()
 			setTradeTableReload(false);
@@ -64,38 +78,38 @@ function HistoryTable() {
 			<div className="max-h-64 overflow-y-auto">
 				<table className="w-full"> */}
 					<tbody className='overflow-y-scroll overflow-x-hidden bg-gray-200'>
-						{positionHistory.data.map((position: { timestamp: number; indexName: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; side: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.PromiseLikeOfReactNode | null | undefined; inputAmount: any; outputAmount: any }, i: React.Key | null | undefined) => {
+						{positionHistoryData.map((position: { timestamp: number; indexName: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; side: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.PromiseLikeOfReactNode | null | undefined; inputAmount: any; outputAmount: any }, i: React.Key | null | undefined) => {
 							return (
 								<>
-								<tr
-									key={i}
-									// className="child-[td]:text-[#D8DBD5]/60 child:px-4 child:text-[10px] bg-[#1C2018]/20"
-									className="text-gray-700 interMedium text-base border-b border-blackText-500"
-								>
-									<td className='px-4 text-left py-3'>{convertTime(position?.timestamp)}</td>
-									
-									{/* <td>{swapToCur.Symbol}</td> */}
-									<td className='px-4 text-left py-3'>
-										{
-											position?.indexName
-										}
-									</td>
-									<td className='px-4 text-left py-3'>
-										<div
-											className={`h-fit w-fit rounded-lg  px-3 py-1 capitalize interBold titleShadow ${position.side === 'Mint Request' ? ' bg-nexLightGreen-500 text-whiteText-500' : 'bg-nexLightRed-500 text-whiteText-500'
-												} flex flex-row items-center justify-center`}
-										>
-											{position.side}
-										</div>
-									</td>
-									<td className='px-4 text-left py-3'>{FormatToViewNumber({value: position.inputAmount, returnType: 'string'})} USD</td>
-									<td className='px-4 text-left py-3'>{FormatToViewNumber({value: position.outputAmount, returnType: 'string'})} USD</td>
-									{/* <td>{Number(position.amount * 1.001)} USD</td> */}
-									{/* <td className="text-left">{position.requestHash}</td> */}
-								</tr>
-								
+									<tr
+										key={i}
+										// className="child-[td]:text-[#D8DBD5]/60 child:px-4 child:text-[10px] bg-[#1C2018]/20"
+										className="text-gray-700 interMedium text-base border-b border-blackText-500"
+									>
+										<td className='px-4 text-left py-3'>{convertTime(position?.timestamp)}</td>
+
+										{/* <td>{swapToCur.Symbol}</td> */}
+										<td className='px-4 text-left py-3'>
+											{
+												position?.indexName
+											}
+										</td>
+										<td className='px-4 text-left py-3'>
+											<div
+												className={`h-fit w-fit rounded-lg  px-3 py-1 capitalize interBold titleShadow ${position.side === 'Mint Request' ? ' bg-nexLightGreen-500 text-whiteText-500' : 'bg-nexLightRed-500 text-whiteText-500'
+													} flex flex-row items-center justify-center`}
+											>
+												{position.side}
+											</div>
+										</td>
+										<td className='px-4 text-left py-3'>{FormatToViewNumber({ value: position.inputAmount, returnType: 'string' })} USD</td>
+										<td className='px-4 text-left py-3'>{FormatToViewNumber({ value: position.outputAmount, returnType: 'string' })} USD</td>
+										{/* <td>{Number(position.amount * 1.001)} USD</td> */}
+										{/* <td className="text-left">{position.requestHash}</td> */}
+									</tr>
+
 								</>
-								
+
 							)
 						})}
 					</tbody>
---
+++ src/types/tradeTableTypes.ts
@@ -0,0 +1,9 @@
+export interface Positions {
+	side: string;
+	user: `0x${string}` | string;
+	tokenAddress: `0x${string}` | string;
+	timestamp: number;
+	inputAmount: number;
+	outputAmount: number;
+	indexName: string;
+}
