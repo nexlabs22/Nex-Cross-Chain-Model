0b08ce64e27888606cb7e7255fd9b5ef940e4663
Naman Thitha <namanthitha01@gmail.com>
2023-12-29 21:07:23 +0530

    API to get top5 cryptos

---
+++ src/app/api/getTop5Cryptos/route.ts
@@ -0,0 +1,26 @@
+import connectToSpotDb from '@/utils/connectToSpotDb'
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET() {
+    const client = await connectToSpotDb();
+    try {
+        const query = `SELECT * from top5crypto order by top5crypto desc limit 1`
+        const result = await client.query(query).then((res => res.rows[0]))
+
+        const cryptoString = result.top5;
+        const cryptoArray = cryptoString.split(',');
+
+        const dataToReturn:{[key:string]: number} = {timestamp: Number(result.timestamp)};
+
+        cryptoArray.forEach((pair:string)=> {
+            const [cryptoName, marketCap] = pair.split(':');
+            dataToReturn[cryptoName] = Number(marketCap);
+        });
+
+        return NextResponse.json({ data: dataToReturn }, { status: 200 })
+    } catch (err) {
+        return NextResponse.json({ message: 'Error in fetching' }, { status: 400 })
+    }finally{
+        await client.end();
+    }
+}
