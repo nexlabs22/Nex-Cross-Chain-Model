3666cadded7f2e78f2644afed3f80ab640b63149
Naman Thitha <namanthitha01@gmail.com>
2024-01-12 17:10:23 +0530

    updated bot api with bybit minTradeValue

--- src/app/api/getCR5Weights/route.ts
+++ src/app/api/getCR5Weights/route.ts
@@ -2,7 +2,7 @@ import connectToSpotDb from '@/utils/connectToSpotDb'
 import { NextResponse, NextRequest } from 'next/server'
 import axios from 'axios'
 
-const cryptoNametoSymbol: { [key: string]: string } = {
+const cryptoNametoSymbol_bitfinex: { [key: string]: string } = {
     'bitcoin': 'btcusd',
     'ethereum': 'ethusd',
     'solana': 'solusd',
@@ -17,11 +17,34 @@ const cryptoNametoSymbol: { [key: string]: string } = {
     'ethereumclassic': 'etcust',
     'xaut': 'xaut:usd',
     'ripple': 'xrpusd'
-    // 'binancecoin': 'bnb:usd', *********
+    // ** Unavailable cryptos
+    // 'binancecoin:', 
     // 'steth:'
-    // 'steller':
-    // 'okb',
-    // 'bitcoincashsv'
+    // 'steller:'
+    // 'okb:',
+    // 'bitcoincashsv:'
+}
+const cryptoNametoSymbol_bybit: { [key: string]: string } = {
+    'bitcoin': 'BTCUSDT',
+    'ethereum': 'ETHUSDT',
+    'solana': 'SOLUSDT',
+    'litecoin': 'LTCUSDT',
+    'binancecoin': 'BNBUSDT',
+    'polkadot': 'DOTUSDT',
+    'cardano': 'ADAUSDT',
+    'eos': 'EOSUSDT',
+    'chainlink': 'LINKUSDT',
+    'dogecoin': 'DOGEUSDT',
+    'bitcoincash': 'BCHUSDT',
+    'ethereumclassic': 'ETCUSDT',
+    'ripple': 'XRPUSDT',
+    'steth':'STETHUSDT',
+    // ** Unavailable cryptos
+    // 'xaut': '',
+    // 'monero': '',
+    // 'steller:',
+    // 'okb:',
+    // 'bitcoincashsv:'
 }
 
 export async function GET() {
@@ -40,16 +63,21 @@ export async function GET() {
             sumOfMarketCap += Number(pair.split(':')[1]);
         });
 
-        const symbolDetails: { pair: string, minimum_order_size: number }[] = await axios.get("https://api.bitfinex.com/v1/symbols_details").then(res => res.data).catch((err) => { console.log(err) })
-        const allocations: { name: string, weight: number, minTradeValueBitfinex: number | string, selectedExchange: string }[] = [];
+        const symbolDetails_bitfinex: { pair: string, minimum_order_size: number }[] = await axios.get("https://api.bitfinex.com/v1/symbols_details").then(res => res.data).catch((err) => { console.log(err) })
+        const symbolDetails_bybit: { name: string, minTradeQty: string }[] = await axios.get("https://api.bybit.com/spot/v3/public/symbols").then(res => res.data.result.list).catch((err) => { console.log(err) })
+        const allocations: { name: string, weight: number, minTradeValueBitfinex: number | string, minTradeValueBybit: number | string, selectedExchange: string }[] = [];
         cryptoArray.forEach((pair: string) => {
             const [cryptoName, marketCap] = pair.split(':');
-            const detail = symbolDetails.filter((data: { pair: string }) => { return cryptoNametoSymbol[cryptoName] === data.pair })[0]
+            const detail_bitfinex = symbolDetails_bitfinex.filter((data: { pair: string }) => { return cryptoNametoSymbol_bitfinex[cryptoName] === data.pair })[0]
+            const detail_bybit = symbolDetails_bybit.filter((data: { name: string }) => { return cryptoNametoSymbol_bybit[cryptoName] === data.name })[0]
+            const minTradeValueBitfinex = cryptoNametoSymbol_bitfinex[cryptoName] ? (detail_bitfinex && detail_bitfinex.minimum_order_size ? Number(detail_bitfinex.minimum_order_size) : 'N/A') : 'Pair does not exist'
+            const minTradeValueBybit = cryptoNametoSymbol_bybit[cryptoName] ? (detail_bybit && detail_bybit.minTradeQty ? Number(detail_bybit.minTradeQty) : 'N/A') : 'Pair does not exist'
             const obj = {
                 name: cryptoName,
                 weight: Number(marketCap) / sumOfMarketCap,
-                minTradeValueBitfinex: cryptoNametoSymbol[cryptoName] ? (detail && detail.minimum_order_size ? Number(detail.minimum_order_size) : 'N/A') : 'Pair does not exist',
-                selectedExchange: 'bitfinex'
+                minTradeValueBitfinex,
+                minTradeValueBybit,
+                selectedExchange: typeof minTradeValueBitfinex === 'number' ? 'bitfinex' : typeof minTradeValueBybit === 'number' ? 'bybit': 'none'
             }
             allocations.push(obj)
         });
