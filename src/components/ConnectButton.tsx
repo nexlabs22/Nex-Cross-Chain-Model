52f967d6c971ddd77eedbdf9720a34f134b4a2be
MedNewton <med.newton.2013@gmail.com>
2024-02-15 08:01:32 -0800

    .

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -93,7 +93,7 @@ const ConnectButton: React.FC<ConnectWalletButton> = ({ tradeNavbarButton }) => 
         },
       });
     }
-  }, [theme]);
+  }, [tradeNavbarButton, selectedTradingCategory]);
 
   
 
