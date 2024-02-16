1ef8cc18e090a0fc84b335db7c772f25e2af354b
Naman Thitha <namanthitha01@gmail.com>
2024-02-16 22:21:28 +0530

    added get weight api

--- src/components/dashboard/TopIndexData.tsx
+++ src/components/dashboard/TopIndexData.tsx
@@ -169,8 +169,9 @@ const TopIndexData = () => {
 
 	async function getANFIWeights() {
 		try {
-			const response = await axios.get(`https://app.nexlabs.io/api/getAnfiWeights`);
-			const RawANFIUnderlyingAssets = response.data.data.allocations;
+			const response = await axios.get('/api/getWeights');
+			console.log("getANFIWeights-->",response)
+			const RawANFIUnderlyingAssets = response.data.anfi;
 
 			// Big logos for POR section : 
 
@@ -208,8 +209,9 @@ const TopIndexData = () => {
 	}
 	async function getCR5Weights() {
 		try {
-			const response = await axios.get(`https://app.nexlabs.io/api/getCR5Weights`);
-			const RawCR5UnderlyingAssets = response.data.data.allocations;
+			const response = await axios.get('/api/getWeights');
+			console.log("getCR5Weights-->",response)
+			const RawCR5UnderlyingAssets = response.data.cr5;
 
 			//Big logos for POR section : 
 			setCR5UnderLyingAssets([]);
