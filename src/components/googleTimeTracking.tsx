bd60f671085032e650183a5162f0717fefb033d7
Naman Thitha <namanthitha01@gmail.com>
2024-04-04 19:03:59 +0530

    google analytics implementations

--- src/components/googleTimeTracking.tsx
+++ src/components/googleTimeTracking.tsx
@@ -3,51 +3,49 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import ReactGA from 'react-ga4' // Import from 'react-ga4' instead of 'react-ga'
 
-const initializeReactGA = () => {
-	ReactGA.initialize('G-NS20Y9N6S5') // Replace 'YOUR_MEASUREMENT_ID' with your actual GA4 Measurement ID
-}
-
-
+// const initializeReactGA = () => {
+// 	ReactGA.initialize('G-NS20Y9N6S5') // Replace 'YOUR_MEASUREMENT_ID' with your actual GA4 Measurement ID
+// }
 
 const TimeTracker: React.FC = () => {
-
+	ReactGA.initialize('G-NS20Y9N6S5')
 	// const address = useAddress()
 
 	const router = useRouter()
 	const location = router.pathname
-	
+
 	const recordUserTime = () => {
 		const startTime = new Date().getTime()
-		console.log("startTime", startTime)
-		
+		console.log('startTime', startTime)
+
 		const recordUserDuration = () => {
 			const duration = Math.round((new Date().getTime() - startTime) / 1000) // Calculate duration in seconds
 
-			console.log("duration", {location, duration})
-			localStorage.setItem("Time", JSON.stringify({location, duration}))
+			console.log('duration', { location, duration })
+			localStorage.setItem('Time', JSON.stringify({ location, duration }))
 
-		// ReactGA.send({
-		//   hitType: 'event',
-		//   eventCategory: 'User',
-		//   eventAction: 'Time Spent',
-		//   eventValue: duration
-		// });
-	
-		ReactGA.event({
-			category: "User",
-			action: "TimeSpent",
-			// label: address, // optional
-			value: duration, // optional, must be a number
-			// nonInteraction: true, // optional, true/false
-			// transport: "xhr", // optional, beacon/xhr/image
-		  });
+			// ReactGA.send({
+			//   hitType: 'event',
+			//   eventCategory: 'User',
+			//   eventAction: 'Time Spent',
+			//   eventValue: duration
+			// });
+
+			ReactGA.event({
+				category: 'User',
+				action: 'TimeSpent',
+				// label: address, // optional
+				value: duration, // optional, must be a number
+				// nonInteraction: true, // optional, true/false
+				// transport: "xhr", // optional, beacon/xhr/image
+			})
 		}
-	
+
 		window.addEventListener('beforeunload', recordUserDuration) // Record time spent when user leaves the page
 	}
 
 	useEffect(() => {
-		initializeReactGA()
+		// initializeReactGA()
 		recordUserTime()
 
 		return () => {
