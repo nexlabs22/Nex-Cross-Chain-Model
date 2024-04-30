d30acf73a16f227b8074ee831cf2b2939a33cf64
MedNewton <med.newton.2013@gmail.com>
2024-04-30 07:45:15 -0700

    .

---
+++ src/components/pwa/PWA3DPieChart.tsx
@@ -0,0 +1,44 @@
+import usePortfolioPageStore from '@/store/portfolioStore';
+import { Chart } from 'react-google-charts'
+
+
+interface PieChart3DProps {
+	data: (string | number)[][]
+}
+
+const PWA3DPieChart: React.FC<PieChart3DProps> = ({ data }) => {
+
+    const {setIndexSelectedInPie} = usePortfolioPageStore();
+
+    const handleSliceClick = (slice: any) => {
+        const index = slice.value[slice.row+1][0]
+        setIndexSelectedInPie(index)
+      };
+
+
+
+	const options = {
+		title: '',
+		legend: { position: 'none' },
+		is3D: true,
+		backgroundColor: 'transparent',
+		colors: ['#91AC9A', '#B7D1D3', '#878787', '#A6C3CE', '#86afbf'],
+	}
+	return (
+		<div className="w-full h-fit min-h-[200px] p-0 flex flex-row items-center justify-center " id='3DPieChartBox'>
+			<Chart
+                chartType="PieChart"
+                className="flex flex-row items-center justify-center p-0 h-fit relative z-[99] "
+                data={data}
+                options={options}
+                width={"100%"}
+                height={"200px"}
+                
+                // chartEvents={chartEvents}
+            />
+			{/* <Chart chartType="ScatterChart" data={data} options={options} graphID="ScatterChart" width="100%" height="400px" chartEvents={()=>{cb}} /> */}
+		</div>
+	)
+}
+
+export default PWA3DPieChart
--- src/pages/pwa_overview/index.tsx
+++ src/pages/pwa_overview/index.tsx
@@ -7,7 +7,7 @@ import PWABottomNav from "@/components/pwa/PWABottomNav";
 import anfiLogo from '@assets/images/anfi.png'
 import cr5Logo from '@assets/images/cr5.png'
 import PWAProfileOverviewHeader from "@/components/pwa/PWAProfileOverviewHeader";
-import PWAPNLChartBox from "@/components/pwa/PWAPNLChartBox";
+import PWA3DChartBox from "@/components/pwa/PWAPNLChartBox";
 import PWAMyAssets from "@/components/pwa/PWAMyAssets";
 import PWABanner from "@/components/pwa/PWABanner";
 import dca from "@assets/images/dca.png"
@@ -22,7 +22,7 @@ export default function PWAOverview() {
         <Box width={"100vw"} height={"fit-content"} display={"flex"} flexDirection={"column"} alignItems={"center"} justifyContent={"start"} paddingY={4} paddingX={3} bgcolor={lightTheme.palette.background.default}>
             <PWATopBar></PWATopBar>
             <PWAProfileOverviewHeader></PWAProfileOverviewHeader>
-            <PWAPNLChartBox></PWAPNLChartBox>
+            <PWA3DChartBox></PWA3DChartBox>
             <PWAMyAssets></PWAMyAssets>
             <PWABanner image={dca.src} bigText="Nex DCA Calculator" smallText="Nex Dollar Cost Averaging (DCA) Calculator, a strategic tool designed for investors." link="" linkText="Learn More"></PWABanner>
             <PWABottomNav></PWABottomNav>
