2f90b74c8ecd5d6733d9c27af84e8c5b22803163
MedNewton <med.newton.2013@gmail.com>
2024-05-17 15:10:35 -0700

    .

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -153,6 +153,9 @@ export default function Portfolio() {
 		indexDetailsMap,
 		uploadedPPLink,
         chosenPPType,
+		isStandalone,
+		browser,
+		os,
 		handleCopyFunction,
 		handleCopyIndexDetailsFunction
 	} = usePortfolio()
@@ -161,59 +164,14 @@ export default function Portfolio() {
 	const { portfolioData, indexSelectedInPie } = usePortfolioPageStore()
 	const [chartType, setChartType] = useState('pie')
 	const { mode } = useLandingPageStore()
+	const { changeSelectedIndex } = useLandingPageStore()
 	
 	
 	
 
 	
 
-	const [isStandalone, setIsStandalone] = useState(false)
-	const [os, setOs] = useState<String>('')
-	const [browser, setBrowser] = useState<String>('')
-
-	function detectMobileBrowserOS() {
-		const userAgent = navigator.userAgent
-
-		let browser: string | undefined
-		let os: string | undefined
-
-		browser = ''
-		os = ''
-		// Check for popular mobile browsers
-		if (/CriOS/i.test(userAgent)) {
-			browser = 'Chrome'
-		} else if (/FxiOS/i.test(userAgent)) {
-			browser = 'Firefox'
-		} else if (/Safari/i.test(userAgent) && !/Chrome/i.test(userAgent)) {
-			browser = 'Safari'
-		}
-
-		// Check for common mobile operating systems
-		if (/iP(ad|hone|od)/i.test(userAgent)) {
-			os = 'iOS'
-		} else if (/Android/i.test(userAgent)) {
-			os = 'Android'
-		}
-
-		setOs(os.toString())
-		setBrowser(browser.toString())
-	}
-
-	useEffect(() => {
-		detectMobileBrowserOS()
-	}, [])
-
-	useEffect(() => {
-		// Client-side detection using window.matchMedia (optional)
-		if (typeof window !== 'undefined') {
-			const mediaQuery = window.matchMedia('(display-mode: standalone)')
-			const handleChange = (event: MediaQueryListEvent) => setIsStandalone(event.matches)
-			mediaQuery.addEventListener('change', handleChange)
-			setIsStandalone(mediaQuery.matches) // Set initial client-side state
-			//alert(mediaQuery.matches)
-			return () => mediaQuery.removeEventListener('change', handleChange)
-		}
-	}, [])
+	
 
 	const PWAProfileOverviewHeader = () => {
 		return (
@@ -276,8 +234,6 @@ export default function Portfolio() {
 			</Stack>
 		)
 	}
-
-	const { changeSelectedIndex } = useLandingPageStore()
 	
 
 	const PWAMyAssets = () => {
