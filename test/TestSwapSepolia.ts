d5212b1abae4fdb62e5792af5ad308515a94d292
Morteza-Khedri <mkhedry3123@gmail.com>
2024-07-16 17:08:41 +0330

    added inputs to the swap script

--- test/TestSwapSepolia.ts
+++ test/TestSwapSepolia.ts
@@ -10,7 +10,7 @@ import { UniswapV3Deployer } from "./uniswap/UniswapV3Deployer";
 import { getMaxTick, getMinTick } from "./uniswap/utils/ticks";
 import { FeeAmount, TICK_SPACINGS } from "./uniswap/utils/constants";
 import { encodePriceSqrt } from "./uniswap/utils/encodePriceSqrt.ts";
-import { seploliaWethAddress, sepoliaBitcoinAddress, sepoliaCrossChainTokenAddress, sepoliaFactoryV3Address, sepoliaRouterV3Address, sepoliaRouterV3Address2 } from "../network";
+import { seploliaWethAddress, sepoliaCrossChainTokenAddress, sepoliaFactoryV3Address, sepoliaRouterV3Address } from "../network";
 // import { WETH9 } from "@uniswap/sdk-core";
 import WETH9 from "./uniswap/utils/WETH9.json";
 import UniswapV3Factory from "@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json"
@@ -20,10 +20,13 @@ import { WethAddresses } from "../contractAddresses";
 const sepoliaARBIndexTokenAddress = "0x2F8B660e8E2CF66889862d4Ab1569cDe98d67748";
 const sepoliaMAG7IndexTokenAddress = "0x955b3F0091414E7DBbe7bdf2c39d73695CDcDd95";
 
-const token0 = WethAddresses[`sepolia`]
 const token1 = sepoliaARBIndexTokenAddress
+const fromETH = false;
+const amountIn = "0.1";
+
 
   describe("Swap", function () {
+    this.timeout(100000); // 100 seconds
     // We define a fixture to reuse the same setup in every test.
     // We use loadFixture to run this setup once, snapshot that state,
     // and reset Hardhat Network to that snapshot in every test.
@@ -49,22 +52,29 @@ const token1 = sepoliaARBIndexTokenAddress
       const unlockTime = (Date.now()) + ONE_YEAR_IN_SECS;
 
       const params1 = {
-        tokenIn: deploymentObj.weth9.address,
-        tokenOut: deploymentObj.token.address,
+        tokenIn: fromETH ? deploymentObj.weth9.address :deploymentObj.token.address,
+        tokenOut: fromETH ? deploymentObj.token.address :deploymentObj.weth9.address,
         fee: FeeAmount.MEDIUM,
         recipient: await deploymentObj.owner.getAddress(),
         deadline: unlockTime,
-        // amountIn: ethers.utils.parseEther("1"),
-        amountIn: ethers.utils.parseEther("0.001"),
+        // amountIn: fromETH ? ethers.utils.parseEther("0.001") :ethers.utils.parseEther("1"),
+        amountIn: ethers.utils.parseEther(amountIn),
         amountOutMinimum:0,
         sqrtPriceLimitX96: 0
       }
       const ownerAddress = deploymentObj.owner.getAddress()
+      if(fromETH){
       console.log("depositing weth...")
-      await deploymentObj.weth9.connect(deploymentObj.owner).deposit({value: ethers.utils.parseEther("0.001")})
+      const depositRes = await deploymentObj.weth9.connect(deploymentObj.owner).deposit({value: ethers.utils.parseEther(amountIn)})
+      await depositRes.wait()
       console.log("approving weth...")
-      await deploymentObj.weth9.connect(deploymentObj.owner).approve(deploymentObj.v3Router.address, ethers.utils.parseEther("0.001"));
-      // await deploymentObj.token.connect(deploymentObj.owner).approve(deploymentObj.v3Router.address, ethers.utils.parseEther("0.01"));
+      const approveRes = await deploymentObj.weth9.connect(deploymentObj.owner).approve(deploymentObj.v3Router.address, ethers.utils.parseEther(amountIn));
+      await approveRes.wait()
+      } else {
+      console.log("approving token...")
+      const approveRes = await deploymentObj.token.connect(deploymentObj.owner).approve(deploymentObj.v3Router.address, ethers.utils.parseEther(amountIn));
+      await approveRes.wait()
+      }
       console.log("swap...")
       const res = await deploymentObj.v3Router.connect(deploymentObj.owner).exactInputSingle(params1, {gasLimit: 1000000});
       const receipt = await res.wait()
