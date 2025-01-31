481669e086a026da238189f95369fb43609ba811
MedNewton <med.newton.2013@gmail.com>
2025-01-31 03:07:50 +0100

    hidden non ready sidebar items

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
             elevation={2}
             sx={{
                 display: { xs: "none", lg: "block" },
-                minWidth: {xs: "4vw", xl: "3vw"},
+                minWidth: { xs: "4vw", xl: "3vw" },
                 width: "fit-content",
                 height: "100vh",
                 maxHeight: "100vh",
@@ -81,17 +81,15 @@ const Sidebar = () => {
                 {
                     settingItems.map((item: NavItem, key: number) => {
                         return (
-                            <Link key={key} href={item.link} width={'fit-content'} display={'flex'} underline="none">
-                                <Stack key={key} direction="row" alignItems={"center"} gap={1}>
-                                    <item.icon size={20} />
-                                    <Typography variant="h6" className="sidebar-link-typography" sx={{
-                                        width: 0,
-                                        overflow: "hidden",
-                                        whiteSpace: "nowrap",
-                                        transition: "width 0.3s ease",
-                                    }}>{item.label}</Typography>
-                                </Stack>
-                            </Link>
+                            <Stack key={key} direction="row" alignItems={"center"} gap={1}>
+                                <item.icon size={20} />
+                                <Typography variant="h6" className="sidebar-link-typography" sx={{
+                                    width: 0,
+                                    overflow: "hidden",
+                                    whiteSpace: "nowrap",
+                                    transition: "width 0.3s ease",
+                                }}>{item.label}</Typography>
+                            </Stack>
                         )
                     })
                 }
