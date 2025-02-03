1ae7ebfe10a9863312ffd8158b1d50e73507fce1
Waltermelon-lang <waltschagen@hotmail.com>
2025-02-03 04:21:32 +0100

    last files update

--- src/components/ui/dashboard/featuredIndices.tsx
+++ src/components/ui/dashboard/featuredIndices.tsx
@@ -1,34 +1,39 @@
-'use client'
+"use client"
 
 import { Stack, Link } from "@mui/material"
-import Grid from '@mui/material/Grid2';
+import Grid from "@mui/material/Grid2"
 import IndexCard from "@/components/ui/generic/indexCard"
-import { TokenObject } from "@/types/indexTypes";
-import { useDashboard } from "@/providers/DashboardProvider";
-import MobileFeaturedIndices from "./mobileFeaturedIndices";
+import { IndexCryptoAsset } from "@/types/indexTypes"
+import { useDashboard } from "@/providers/DashboardProvider"
+import MobileFeaturedIndices from "./mobileFeaturedIndices"
 
 const FeaturedIndices = () => {
-    
-    const { nexTokens} = useDashboard()
+  const { nexTokens } = useDashboard()
 
-    return (
-        <>
-        <Stack width="100%" display={{xs: 'none', lg: 'block'}}>
-            <Grid container spacing={2}>
-                {
-                    nexTokens.slice(0, 3).map((index: TokenObject, key: number) => (
-                        <Grid size={{ xs: 12, lg: 4 }} key={key}>
-                            <Link href={`/trade?side=buy&index=${index.symbol}`} key={key} style={{ textDecoration: 'none', width: '100%', cursor: 'pointer' }}>
-                                <IndexCard index={index} />
-                            </Link>
-                        </Grid>
-                    ))
-                }
+  return (
+    <>
+      <Stack width="100%" display={{ xs: "none", lg: "block" }}>
+        <Grid container spacing={2}>
+          {nexTokens.slice(0, 3).map((index: IndexCryptoAsset, key: number) => (
+            <Grid size={{ xs: 12, lg: 4 }} key={key}>
+              <Link
+                href={`/trade?side=buy&index=${index.symbol}`}
+                key={key}
+                style={{
+                  textDecoration: "none",
+                  width: "100%",
+                  cursor: "pointer",
+                }}
+              >
+                <IndexCard index={index} />
+              </Link>
             </Grid>
-        </Stack>
-        <MobileFeaturedIndices />
-        </>
-    )
+          ))}
+        </Grid>
+      </Stack>
+      <MobileFeaturedIndices />
+    </>
+  )
 }
 
-export default FeaturedIndices;
+export default FeaturedIndices
