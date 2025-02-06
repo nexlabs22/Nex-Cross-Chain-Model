88f0da9b8571869381b5e63f447e929eb228587a
MedNewton <med.newton.2013@gmail.com>
2025-02-07 00:45:51 +0100

    .

--- src/app/trade/page.tsx
+++ src/app/trade/page.tsx
@@ -1,31 +1,26 @@
-"use client"
+'use client'
 
-// src/app/trade/[operation]/[index]/page.tsx
-
-import { Box, Typography, Stack } from "@mui/material"
-import Grid from "@mui/material/Grid2"
-import Header from "@/components/layout/Header"
-import Sidebar from "@/components/layout/sidebar"
-import Footer from "@/components/layout/Footer"
-import GenericCard from "@/components/ui/generic/genericCard"
-import Swap from "@/components/ui/trade/swap"
-import TabbedTablesView from "@/components/ui/trade/tabbedViewTables"
-import { useDashboard } from "@/providers/DashboardProvider"
+import { Typography } from "@mui/material";
+import Grid from '@mui/material/Grid2';
+import GenericCard from "@/components/ui/generic/genericCard";
+import Swap from "@/components/ui/trade/swap";
+import TabbedTablesView from '@/components/ui/trade/tabbedViewTables';
+import { useDashboard } from "@/providers/DashboardProvider";
 
 const Page = ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined }
 }) => {
-  const {  nexTokens } = useDashboard()
-  const { side, index } = searchParams
-  let selectedIndex = nexTokens[0]
+  const { nexTokens } = useDashboard()
+  const { side, index } = searchParams;
+  let defaultToken = nexTokens[0];
   if (index) {
-    const foundToken = nexTokens.find((token) => token.symbol === index)
-    selectedIndex = foundToken || nexTokens[0]
+    const foundToken = nexTokens.find(token => token.symbol === index);
+    defaultToken = foundToken || nexTokens[0];
   }
 
-  const selectedSide = side && side === "buy" ? "buy" : "sell"
+  const selectedSide = side && side === 'buy' ? 'buy' : 'sell';
 
   return (
     <>
@@ -38,12 +33,12 @@ const Page = ({
           </GenericCard>
         </Grid>
         <Grid size={{ xs: 12, sm: 4 }}>
-          <Swap side={selectedSide} selectedIndex={selectedIndex} />
+          <Swap side={selectedSide} defaultToken={defaultToken} />
         </Grid>
       </Grid>
       <TabbedTablesView />
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
