82d0346fe959834e70dd61f255a7ce1d1de8f495
MedNewton <med.newton.2013@gmail.com>
2025-02-10 15:06:21 +0100

    small deployment blocking issues

--- src/app/index-details/layout.tsx
+++ src/app/index-details/layout.tsx
@@ -1,8 +1,4 @@
 import type { Metadata } from "next"
-import { Box, Stack } from "@mui/material"
-import Header from "@/components/layout/Header"
-import Footer from "@/components/layout/Footer"
-import Sidebar from "@/components/layout/sidebar"
 import { Suspense } from "react"
 export const metadata: Metadata = {
   title: "Nexlabs | Index Details",
