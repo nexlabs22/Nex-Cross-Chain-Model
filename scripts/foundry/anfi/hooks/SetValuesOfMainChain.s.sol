c7475b02d634171363a31caa9d0b726bbab1664a
soheilprs <soheilparsaei19@protonmail.com>
2025-02-10 13:33:52 -0800

    Solved stack too-deep error

--- scripts/foundry/anfi/hooks/SetValuesOfMainChain.s.sol
+++ scripts/foundry/anfi/hooks/SetValuesOfMainChain.s.sol
@@ -12,26 +12,7 @@ import "../../../../contracts/factory/BalancerSender.sol";
 import "../../../../contracts/factory/FunctionsOracle.sol";
 
 contract CombinedSetValuesScript is Script, Test {
-    function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
-
-        string memory targetChain = "sepolia";
-
-        _setIndexFactoryStorageValues(targetChain);
-
-        _setIndexTokenValues(targetChain);
-
-        _setMockFillAssetsList(targetChain);
-
-        _setFunctionsOracle(targetChain);
-
-        vm.stopBroadcast();
-
-        console.log("All set functions completed successfully!");
-    }
-
-    function _setIndexFactoryStorageValues(string memory targetChain) internal {
+    struct FactoryStorageEnv {
         address indexFactoryStorageProxy;
         address mainCrossChainTokenAddress;
         address otherCrossChainTokenAddress;
@@ -45,61 +26,107 @@ contract CombinedSetValuesScript is Script, Test {
         address indexFactoryBalancerProxy;
         address coreSenderProxy;
         address balancerSenderProxy;
-        address functionsOracleProxy;
+    }
 
+    struct MockFillEnv {
+        uint64 mainChainSelector;
+        uint64 otherChainSelector;
+        address wethAddress;
+        address functionsOracleProxy;
+        bool isTestnet;
+    }
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        string memory targetChain = "sepolia";
+
+        // 1. setIndexFactoryStorageValues
+        _setIndexFactoryStorageValues(targetChain);
+
+        // 2. setIndexTokenValues
+        _setIndexTokenValues(targetChain);
+
+        // 3. setMockFillAssetsList
+        _setMockFillAssetsList(targetChain);
+
+        // 4. setFunctionsOracle
+        _setFunctionsOracle(targetChain);
+
+        vm.stopBroadcast();
+
+        console.log("All set functions completed successfully!");
+    }
+
+    function _setIndexFactoryStorageValues(string memory targetChain) internal {
+        FactoryStorageEnv memory data = _readIndexFactoryStorageEnvVars(targetChain);
+
+        _applyIndexFactoryStorageValues(data);
+
+        console.log("Done: _setIndexFactoryStorageValues()");
+    }
+
+    function _readIndexFactoryStorageEnvVars(string memory targetChain)
+        internal
+        view
+        returns (FactoryStorageEnv memory data)
+    {
         if (keccak256(bytes(targetChain)) == keccak256("sepolia")) {
-            indexFactoryStorageProxy = vm.envAddress("SEPOLIA_INDEX_FACTORY_STORAGE_PROXY_ADDRESS");
-            mainCrossChainTokenAddress = vm.envAddress("SEPOLIA_CROSS_CHAIN_TOKEN_ADDRESS");
-            otherCrossChainTokenAddress = vm.envAddress("ARBITRUM_SEPOLIA_CROSS_CHAIN_TOKEN_ADDRESS");
-            crossChainFactoryProxy = vm.envAddress("ARBITRUM_SEPOLIA_CROSS_CHAIN_FACTORY_PROXY_ADDRESS");
-            weth = vm.envAddress("SEPOLIA_WETH_ADDRESS");
-            mainChainSelector = uint64(vm.envUint("SEPOLIA_CHAIN_SELECTOR"));
-            otherChainSelector = uint64(vm.envUint("ARBITRUM_SEPOLIA_CHAIN_SELECTOR"));
-            indexFactoryProxy = vm.envAddress("SEPOLIA_INDEX_FACTORY_PROXY_ADDRESS");
-            priceOracle = vm.envAddress("SEPOLIA_PRICE_ORACLE");
-            vaultProxy = vm.envAddress("SEPOLIA_VAULT_PROXY_ADDRESS");
-            indexFactoryBalancerProxy = vm.envAddress("SEPOLIA_INDEX_FACTORY_BALANCER_PROXY_ADDRESS");
-            coreSenderProxy = vm.envAddress("SEPOLIA_CORE_SENDER_PROXY_ADDRESS");
-            balancerSenderProxy = vm.envAddress("SEPOLIA_BALANCER_SENDER_PROXY_ADDRESS");
+            data.indexFactoryStorageProxy = vm.envAddress("SEPOLIA_INDEX_FACTORY_STORAGE_PROXY_ADDRESS");
+            data.mainCrossChainTokenAddress = vm.envAddress("SEPOLIA_CROSS_CHAIN_TOKEN_ADDRESS");
+            data.otherCrossChainTokenAddress = vm.envAddress("ARBITRUM_SEPOLIA_CROSS_CHAIN_TOKEN_ADDRESS");
+            data.crossChainFactoryProxy = vm.envAddress("ARBITRUM_SEPOLIA_CROSS_CHAIN_FACTORY_PROXY_ADDRESS");
+            data.weth = vm.envAddress("SEPOLIA_WETH_ADDRESS");
+            data.mainChainSelector = uint64(vm.envUint("SEPOLIA_CHAIN_SELECTOR"));
+            data.otherChainSelector = uint64(vm.envUint("ARBITRUM_SEPOLIA_CHAIN_SELECTOR"));
+            data.indexFactoryProxy = vm.envAddress("SEPOLIA_INDEX_FACTORY_PROXY_ADDRESS");
+            data.priceOracle = vm.envAddress("SEPOLIA_PRICE_ORACLE");
+            data.vaultProxy = vm.envAddress("SEPOLIA_VAULT_PROXY_ADDRESS");
+            data.indexFactoryBalancerProxy = vm.envAddress("SEPOLIA_INDEX_FACTORY_BALANCER_PROXY_ADDRESS");
+            data.coreSenderProxy = vm.envAddress("SEPOLIA_CORE_SENDER_PROXY_ADDRESS");
+            data.balancerSenderProxy = vm.envAddress("SEPOLIA_BALANCER_SENDER_PROXY_ADDRESS");
         } else if (keccak256(bytes(targetChain)) == keccak256("arbitrum_mainnet")) {
-            indexFactoryStorageProxy = vm.envAddress("ARBITRUM_INDEX_FACTORY_STORAGE_PROXY_ADDRESS");
-            mainCrossChainTokenAddress = vm.envAddress("ARBITRUM_CROSS_CHAIN_TOKEN_ADDRESS");
-            otherCrossChainTokenAddress = vm.envAddress("ETHEREUM_CROSS_CHAIN_TOKEN_ADDRESS");
-            crossChainFactoryProxy = vm.envAddress("ETHEREUM_CROSS_CHAIN_FACTORY_PROXY_ADMIN_ADDRESS");
-            weth = vm.envAddress("ARBITRUM_WETH_ADDRESS");
-            mainChainSelector = uint64(vm.envUint("ARBITRUM_CHAIN_SELECTOR"));
-            otherChainSelector = uint64(vm.envUint("ETHEREUM_CHAIN_SELECTOR"));
-            indexFactoryProxy = vm.envAddress("ARBITRUM_INDEX_FACTORY_PROXY_ADDRESS");
-            priceOracle = vm.envAddress("ARBITRUM_PRICE_ORACLE");
-            vaultProxy = vm.envAddress("ARBITRUM_VAULT_PROXY_ADDRESS");
-            indexFactoryBalancerProxy = vm.envAddress("ARBITRUM_INDEX_FACTORY_BALANCER_PROXY_ADDRESS");
-            coreSenderProxy = vm.envAddress("ARBITRUM_CORE_SENDER_PROXY_ADDRESS");
-            balancerSenderProxy = vm.envAddress("ARBITRUM_BALANCER_SENDER_PROXY_ADDRESS");
+            data.indexFactoryStorageProxy = vm.envAddress("ARBITRUM_INDEX_FACTORY_STORAGE_PROXY_ADDRESS");
+            data.mainCrossChainTokenAddress = vm.envAddress("ARBITRUM_CROSS_CHAIN_TOKEN_ADDRESS");
+            data.otherCrossChainTokenAddress = vm.envAddress("ETHEREUM_CROSS_CHAIN_TOKEN_ADDRESS");
+            data.crossChainFactoryProxy = vm.envAddress("ETHEREUM_CROSS_CHAIN_FACTORY_PROXY_ADMIN_ADDRESS");
+            data.weth = vm.envAddress("ARBITRUM_WETH_ADDRESS");
+            data.mainChainSelector = uint64(vm.envUint("ARBITRUM_CHAIN_SELECTOR"));
+            data.otherChainSelector = uint64(vm.envUint("ETHEREUM_CHAIN_SELECTOR"));
+            data.indexFactoryProxy = vm.envAddress("ARBITRUM_INDEX_FACTORY_PROXY_ADDRESS");
+            data.priceOracle = vm.envAddress("ARBITRUM_PRICE_ORACLE");
+            data.vaultProxy = vm.envAddress("ARBITRUM_VAULT_PROXY_ADDRESS");
+            data.indexFactoryBalancerProxy = vm.envAddress("ARBITRUM_INDEX_FACTORY_BALANCER_PROXY_ADDRESS");
+            data.coreSenderProxy = vm.envAddress("ARBITRUM_CORE_SENDER_PROXY_ADDRESS");
+            data.balancerSenderProxy = vm.envAddress("ARBITRUM_BALANCER_SENDER_PROXY_ADDRESS");
         } else {
             revert("Unsupported target chain (FactoryStorageValues)");
         }
+    }
 
+    function _applyIndexFactoryStorageValues(FactoryStorageEnv memory data) internal {
         uint24[] memory feesData = new uint24[](1);
         feesData[0] = 3000;
 
         address[] memory path = new address[](2);
-        path[0] = weth;
-        path[1] = mainCrossChainTokenAddress;
+        path[0] = data.weth;
+        path[1] = data.mainCrossChainTokenAddress;
 
-        IndexFactoryStorage(indexFactoryStorageProxy).setCrossChainToken(
-            otherChainSelector, mainCrossChainTokenAddress, path, feesData
+        IndexFactoryStorage(data.indexFactoryStorageProxy).setCrossChainToken(
+            data.otherChainSelector, data.mainCrossChainTokenAddress, path, feesData
         );
 
-        IndexFactoryStorage(indexFactoryStorageProxy).setCrossChainFactory(crossChainFactoryProxy, otherChainSelector);
+        IndexFactoryStorage(data.indexFactoryStorageProxy).setCrossChainFactory(
+            data.crossChainFactoryProxy, data.otherChainSelector
+        );
 
-        IndexFactoryStorage(indexFactoryStorageProxy).setIndexFactory(indexFactoryProxy);
-        IndexFactoryStorage(indexFactoryStorageProxy).setPriceOracle(priceOracle);
-        IndexFactoryStorage(indexFactoryStorageProxy).setVault(vaultProxy);
-        IndexFactoryStorage(indexFactoryStorageProxy).setIndexFactoryBalancer(indexFactoryBalancerProxy);
-        IndexFactoryStorage(indexFactoryStorageProxy).setCoreSender(coreSenderProxy);
-        IndexFactoryStorage(indexFactoryStorageProxy).setBalancerSender(balancerSenderProxy);
-
-        console.log("Done: _setIndexFactoryStorageValues()");
+        IndexFactoryStorage(data.indexFactoryStorageProxy).setIndexFactory(data.indexFactoryProxy);
+        IndexFactoryStorage(data.indexFactoryStorageProxy).setPriceOracle(data.priceOracle);
+        IndexFactoryStorage(data.indexFactoryStorageProxy).setVault(data.vaultProxy);
+        IndexFactoryStorage(data.indexFactoryStorageProxy).setIndexFactoryBalancer(data.indexFactoryBalancerProxy);
+        IndexFactoryStorage(data.indexFactoryStorageProxy).setCoreSender(data.coreSenderProxy);
+        IndexFactoryStorage(data.indexFactoryStorageProxy).setBalancerSender(data.balancerSenderProxy);
     }
 
     function _setIndexTokenValues(string memory targetChain) internal {
@@ -122,31 +149,45 @@ contract CombinedSetValuesScript is Script, Test {
     }
 
     function _setMockFillAssetsList(string memory targetChain) internal {
-        address indexFactoryStorageProxy;
-        uint64 mainChainSelector;
-        uint64 otherChainSelector;
-        address wethAddress;
-        address functionsOracleProxy;
+        MockFillEnv memory data = _readMockFillAssetsListEnvVars(targetChain);
 
+        _applyMockFillAssetsList(data);
+
+        console.log("Done: _setMockFillAssetsList()");
+    }
+
+    function _readMockFillAssetsListEnvVars(string memory targetChain)
+        internal
+        view
+        returns (MockFillEnv memory data)
+    {
         if (keccak256(bytes(targetChain)) == keccak256("sepolia")) {
-            mainChainSelector = uint64(vm.envUint("SEPOLIA_CHAIN_SELECTOR"));
-            otherChainSelector = uint64(vm.envUint("ARBITRUM_SEPOLIA_CHAIN_SELECTOR"));
-            wethAddress = vm.envAddress("SEPOLIA_WETH_ADDRESS");
-            functionsOracleProxy = vm.envAddress("SEPOLIA_FUNCTIONS_ORACLE_PROXY_ADDRESS");
-
-            _fillMockAssetsListTestnet(functionsOracleProxy, mainChainSelector, otherChainSelector, wethAddress);
+            data.mainChainSelector = uint64(vm.envUint("SEPOLIA_CHAIN_SELECTOR"));
+            data.otherChainSelector = uint64(vm.envUint("ARBITRUM_SEPOLIA_CHAIN_SELECTOR"));
+            data.wethAddress = vm.envAddress("SEPOLIA_WETH_ADDRESS");
+            data.functionsOracleProxy = vm.envAddress("SEPOLIA_FUNCTIONS_ORACLE_PROXY_ADDRESS");
+            data.isTestnet = true;
         } else if (keccak256(bytes(targetChain)) == keccak256("arbitrum_mainnet")) {
-            mainChainSelector = uint64(vm.envUint("ARBITRUM_CHAIN_SELECTOR"));
-            otherChainSelector = uint64(vm.envUint("ETHEREUM_CHAIN_SELECTOR"));
-            wethAddress = vm.envAddress("ARBITRUM_WETH_ADDRESS");
-            functionsOracleProxy = vm.envAddress("ARBITRUM_FUNCTIONS_ORACLE_PROXY_ADDRESS");
-
-            _fillMockAssetsListMainnet(functionsOracleProxy, mainChainSelector, otherChainSelector, wethAddress);
+            data.mainChainSelector = uint64(vm.envUint("ARBITRUM_CHAIN_SELECTOR"));
+            data.otherChainSelector = uint64(vm.envUint("ETHEREUM_CHAIN_SELECTOR"));
+            data.wethAddress = vm.envAddress("ARBITRUM_WETH_ADDRESS");
+            data.functionsOracleProxy = vm.envAddress("ARBITRUM_FUNCTIONS_ORACLE_PROXY_ADDRESS");
+            data.isTestnet = false;
         } else {
             revert("Unsupported target chain (MockFillAssetsList)");
         }
+    }
 
-        console.log("Done: _setMockFillAssetsList()");
+    function _applyMockFillAssetsList(MockFillEnv memory data) internal {
+        if (data.isTestnet) {
+            _fillMockAssetsListTestnet(
+                data.functionsOracleProxy, data.mainChainSelector, data.otherChainSelector, data.wethAddress
+            );
+        } else {
+            _fillMockAssetsListMainnet(
+                data.functionsOracleProxy, data.mainChainSelector, data.otherChainSelector, data.wethAddress
+            );
+        }
     }
 
     function _fillMockAssetsListMainnet(
@@ -166,7 +207,6 @@ contract CombinedSetValuesScript is Script, Test {
         uint24[] memory feesData = new uint24[](1);
         feesData[0] = 3000;
 
-        // chainSelectors array
         uint64[] memory chainSelectors = new uint64[](2);
         chainSelectors[0] = mainChainSelector;
         chainSelectors[1] = otherChainSelector;
@@ -180,7 +220,6 @@ contract CombinedSetValuesScript is Script, Test {
         }
 
         FunctionsOracle(functionsOracleProxy).mockFillAssetsList(assetList, pathData, marketShares, chainSelectors);
-
         console.log("Called mockFillAssetsList() [mainnet style].");
     }
 
@@ -205,7 +244,7 @@ contract CombinedSetValuesScript is Script, Test {
         chainSelectors[0] = mainChainSelector;
         chainSelectors[1] = otherChainSelector;
 
-        bytes[] memory pathData = new bytes[](11);
+        bytes[] memory pathData = new bytes[](2);
         for (uint256 i = 0; i < 2; i++) {
             address[] memory path = new address[](2);
             path[0] = wethAddress;
@@ -214,7 +253,6 @@ contract CombinedSetValuesScript is Script, Test {
         }
 
         FunctionsOracle(functionsOracleProxy).mockFillAssetsList(assetList, pathData, marketShares, chainSelectors);
-
         console.log("Called mockFillAssetsList() [testnet style].");
     }
 
@@ -237,5 +275,7 @@ contract CombinedSetValuesScript is Script, Test {
 
         FunctionsOracle(functionsOracleProxy).setIndexFactoryBalancer(indexFactoryBalancerProxy);
         FunctionsOracle(functionsOracleProxy).setBalancerSender(balancerSenderProxy);
+
+        console.log("Done: _setFunctionsOracle()");
     }
 }
