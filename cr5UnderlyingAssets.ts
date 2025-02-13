e6d72542b492616d317081e58d9c146f1ed64eb8
Morteza3123 <mkhedry3123@gmail.com>
2025-02-14 01:14:36 +0330

    added underlying assets info and tested deployment scripts

---
+++ cr5UnderlyingAssets.ts
@@ -0,0 +1,38 @@
+
+const data = {
+    "bitcoin" : {
+        "address": "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
+        "weight": 72,
+        "chain": "arbitrum",
+        "path" : "[weth, wbtc]",
+        "fees" : "[3000]"
+    },
+    "weth" : {
+        "address": "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+        "weight": 14,
+        "chain": "arbitrum",
+        "path" : "[weth, weth]",
+        "fees" : "[3000]"
+    },
+    "xrp" : {
+        "address": "0x1d2f0da169ceb9fc7b3144628db156f3f6c60dbe",
+        "weight": 6,
+        "chain": "binance",
+        "path" : "[wbnb, xrp]",
+        "fees" : "[2500]"
+    },
+    "sol" : {
+        "address": "0x570a5d26f7765ecb712c0924e4de545b89fd43df",
+        "weight": 4,
+        "chain": "binance",
+        "path" : "[wbnb, sol]",
+        "fees" : "[2500]"
+    },
+    "wbnb" : {
+        "address": "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+        "weight": 4,
+        "chain": "binance",
+        "path" : "[wbnb, wbnb]",
+        "fees" : "[3000]"
+    },
+}
