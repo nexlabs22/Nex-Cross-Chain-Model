fb1d73a1c30b73974942a6a6e785f87682f6d922
MedNewton <med.newton.2013@gmail.com>
2025-02-26 14:38:00 +0100

    .

--- src/components/ui/generic/vaultScale.tsx
+++ src/components/ui/generic/vaultScale.tsx
@@ -13,25 +13,21 @@ const VaultScale = ({ data }: VaultScaleProps) => {
     const theme = useTheme();
     const sortedData = useMemo(() => [...data].sort((a, b) => b.weight - a.weight), [data]);
     const colors = ['#EF8E19', '#5F7AE3', '#006CA5'];
-    const coloredWidth = 3; // Fixed width for colored ticks
-    const gapWidth = 5; // Fixed width for transparent gaps
+    const coloredWidth = 3;
+    const gapWidth = 5;
 
-    // State to track the hovered tick
     const [hoveredIndex, setHoveredIndex] = useState<{ segment: number; unit: number } | null>(null);
 
     return (
         <Stack width="100%" spacing={1}>
             <Stack width="100%" direction="row" height={250} alignItems="flex-end">
                 {sortedData.map((item, segmentIndex) => {
-                    // Calculate total segment width in pixels
                     const containerWidth = document.documentElement.clientWidth || window.innerWidth;
                     const segmentWidthPx = (item.weight / 100) * containerWidth;
 
-                    // Calculate number of tick-gap pairs needed
                     const totalUnitWidth = coloredWidth + gapWidth;
                     const units = Math.floor(segmentWidthPx / totalUnitWidth);
 
-                    // Calculate exact width needed to prevent subpixels
                     const exactSegmentWidth = units * totalUnitWidth;
 
                     return (
@@ -45,15 +41,14 @@ const VaultScale = ({ data }: VaultScaleProps) => {
                             alignItems="flex-end"
                             position="relative"
                         >
-                            {/* Label and Logo */}
                             <Stack
                                 direction="row"
                                 alignItems="center"
                                 justifyContent="center"
                                 gap={2}
                                 position="absolute"
-                                left="10px"
-                                bottom="40%"
+                                left="3%"
+                                bottom="45%"
                             >
                                 <Box width={40} height={40} borderRadius="50%">
                                     {item.index.logoComponent}
@@ -68,7 +63,6 @@ const VaultScale = ({ data }: VaultScaleProps) => {
                                 </Stack>
                             </Stack>
 
-                            {/* Ticks and Gaps */}
                             <Box
                                 display="flex"
                                 flexDirection="row"
@@ -77,7 +71,6 @@ const VaultScale = ({ data }: VaultScaleProps) => {
                                 width={`${exactSegmentWidth}px`}
                             >
                                 {[...Array(units)].map((_, unitIndex) => {
-                                    // Check if the current tick is hovered or adjacent to the hovered tick
                                     const isHovered = hoveredIndex?.segment === segmentIndex && hoveredIndex?.unit === unitIndex;
                                     const isPrev = hoveredIndex?.segment === segmentIndex && hoveredIndex?.unit === unitIndex - 1;
                                     const isNext = hoveredIndex?.segment === segmentIndex && hoveredIndex?.unit === unitIndex + 1;
@@ -93,26 +86,19 @@ const VaultScale = ({ data }: VaultScaleProps) => {
                                             onMouseEnter={() => setHoveredIndex({ segment: segmentIndex, unit: unitIndex })}
                                             onMouseLeave={() => setHoveredIndex(null)}
                                         >
-                                            {/* Colored Tick */}
                                             <Box
                                                 className="tick"
                                                 width={`${coloredWidth}px`}
                                                 height={
                                                     isHovered && unitIndex !== 0 ? '35%' :
                                                     (isPrev || isNext) && unitIndex !== 0 ? '32%' :
-                                                    unitIndex === 0 ? '100%' : '30%'
+                                                    unitIndex === 0 ? '85%' : '30%'
                                                 }
                                                 bgcolor={colors[segmentIndex % colors.length]}
                                                 sx={{
-                                                    transition: 'height 0.2s ease, opacity 0.2s ease',
-                                                    opacity: hoveredIndex ? (
-                                                        isHovered ? 1 : // Hovered tick: 100% opacity
-                                                        isPrev || isNext ? 0.8 : // Adjacent ticks: 80% opacity
-                                                        0.5 // All other ticks: 50% opacity
-                                                    ) : 1, // Default: 100% opacity
+                                                    transition: 'height 0.2s ease',
                                                 }}
                                             />
-                                            {/* Transparent Gap */}
                                             <Box
                                                 className="gap"
                                                 width={`${gapWidth}px`}
