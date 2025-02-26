6c59dcb6d26f5d22ad5c7555e86e585bb5384be2
MedNewton <med.newton.2013@gmail.com>
2025-02-26 14:32:36 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,80 +2,12 @@ import type { Metadata } from "next"
 import ClientThemeProvider from "@/providers/ThemeProvider"
 import { ThirdwebProvider } from "thirdweb/react"
 import InitColorSchemeScript from "@mui/material/InitColorSchemeScript"
+import "@/fonts/satoshi/satoshi.css"
 import { DashboardProvider } from "@/providers/DashboardProvider"
 import { GlobalProvider } from "@/providers/GlobalProvider"
 import { TradeProvider } from "@/providers/TradeProvider"
 import { HistoryProvider } from "@/providers/HistoryProvider"
-import { ToastContainer } from "react-toastify"
-import localFont from "next/font/local"
-
-// Define local font optimization
-const satoshi = localFont({
-  src: [
-    {
-      path: "../fonts/satoshi/Satoshi-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-LightItalic.woff2",
-      weight: "300",
-      style: "italic",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-Italic.woff2",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-MediumItalic.woff2",
-      weight: "500",
-      style: "italic",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-BoldItalic.woff2",
-      weight: "700",
-      style: "italic",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-Black.woff2",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-BlackItalic.woff2",
-      weight: "900",
-      style: "italic",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-Variable.woff2",
-      weight: "300 900",
-      style: "normal",
-    },
-    {
-      path: "../fonts/satoshi/Satoshi-VariableItalic.woff2",
-      weight: "300 900",
-      style: "italic",
-    },
-  ],
-  variable: "--font-satoshi",
-  display: "swap",
-})
+import { ToastContainer } from 'react-toastify';
 
 export const metadata: Metadata = {
   title: "NexLabs - Index your trades, your investment, your future",
@@ -90,8 +22,8 @@ export default function RootLayout({
 }>) {
   console.log("running root layout")
   return (
-    <html lang="en" className={satoshi.variable} suppressHydrationWarning>
-      <body style={{ overflowX: "hidden" }}>
+    <html lang="en" suppressHydrationWarning>
+      <body style={{ overflowX: "hidden", fontFamily: "Satoshi-Variable" }}>
         <ThirdwebProvider>
           <GlobalProvider>
             <DashboardProvider>
